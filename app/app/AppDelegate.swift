d017b159ff3c6c725a39e008d0f211bc2b11598b
chris <chrstphr@outlook.com>
2016-11-29 14:21:30 +1030

    I have setup a basic login page consisting of a text field for patient's to enter their unique ID, and a login button to redirect them to the app.

--- app/app/AppDelegate.swift
+++ app/app/AppDelegate.swift
@@ -15,6 +15,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        /////////////////////
+        //  CREATE WINDOW  //
+        /////////////////////
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        
+        ////////////////////////////////
+        //  SET ROOT VIEW CONTROLLER  //
+        ////////////////////////////////
+        
+        window?.rootViewController = LoginController()
+        
         // Override point for customization after application launch.
         return true
     }
