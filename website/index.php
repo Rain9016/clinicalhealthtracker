387e0b818df95f11cdba2b488cd77d75743b1bcd
chris <chrstphr@outlook.com>
2016-12-23 11:37:37 +1030

    Add basic website index

---
+++ website/index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Clinical Health Tracker</title>
+<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
+<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Lobster">
+<style type="text/css">
+    *, :before, :after {
+        margin: 0;
+        padding: 0;
+    }
+
+    html {
+    }
+
+    body {
+        font-family: 'Open Sans', sans-serif;
+    }
+
+    @media (min-width : 320px) {
+    }
+
+    @media (min-width : 768px) and (max-width : 1024px) {
+
+    }
+</style>
+</head>
+<body>
+<form method="post" action="login.php">
+welcome to health tracker<br>
+<input type="text" placeholder="username" name="username" required><br>
+<input type"password" placeholder="password" name="password" required><br>
+<button type="submit">log in</button> or <a href="signup.php">sign up a patient</a>
+</form>
+</body>
+</html>
