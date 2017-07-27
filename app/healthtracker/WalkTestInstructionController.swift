d6022d9092d4644ff53b8b85ede25f8962392ba9
chris <chris@chriss-MacBook.local>
2017-07-28 01:01:09 +0930

    Change walk test instructions, text and voice

--- app/healthtracker/WalkTestInstructionController.swift
+++ app/healthtracker/WalkTestInstructionController.swift
@@ -9,7 +9,6 @@
 import UIKit
 
 class WalkTestInstructionController: UIViewController {
-    var page: Int!
     var audioManager = AudioManager.sharedInstance
     var timer = Timer()
     
@@ -35,34 +34,6 @@ class WalkTestInstructionController: UIViewController {
         label.lineBreakMode = NSLineBreakMode.byWordWrapping
         return label
     }()
-
-    let button: UIButton = {
-        let button = UIButton()
-        button.backgroundColor = UIColor.white
-        button.setTitleColor(UIColor.init(r: 204, g: 0, b: 0), for: .normal)
-        button.layer.borderWidth = 1
-        button.layer.borderColor = UIColor.init(r: 204, g: 0, b: 0).cgColor
-        button.layer.cornerRadius = 4
-        
-        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
-        return button
-    }()
-    
-    func buttonAction() {
-        guard (page != 1) else {
-            dismiss(animated: true, completion: nil)
-            return
-        }
-        
-        /* set the back bar button item */
-        let backBarButtonItem = UIBarButtonItem()
-        backBarButtonItem.title = "Back"
-        navigationItem.backBarButtonItem = backBarButtonItem
-        
-        let instructionController = WalkTestInstructionController()
-        instructionController.page = self.page + 1
-        self.navigationController?.pushViewController(instructionController, animated: true)
-    }
     
     let cancel_button: UIBarButtonItem = {
         let barButtonItem = UIBarButtonItem()
@@ -77,21 +48,11 @@ class WalkTestInstructionController: UIViewController {
     }
     
     func playNext() {
-        if (page == 0) {
-            if !audioManager.isPlaying() {
-                audioManager.playAudio(name: "instruction-p1-pt2")
+        if !audioManager.isPlaying() {
+            audioManager.playAudio(name: "instruction-pt2")
                 
-                if (timer.isValid) {
-                    timer.invalidate()
-                }
-            }
-        } else if (page == 1) {
-            if !audioManager.isPlaying() {
-                audioManager.playAudio(name: "instruction-p2-pt2")
-                
-                if (timer.isValid) {
-                    timer.invalidate()
-                }
+            if (timer.isValid) {
+                timer.invalidate()
             }
         }
     }
@@ -132,57 +93,35 @@ class WalkTestInstructionController: UIViewController {
         contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
         
         contentView.addSubview(label)
-        contentView.addSubview(button)
         
         label.translatesAutoresizingMaskIntoConstraints = false
         label.widthAnchor.constraint(equalTo: contentView.widthAnchor, constant: -40).isActive = true
         label.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 20).isActive = true
         label.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
         
-        switch page {
-        case 0:
-            label.text = "The object of this test, is to walk as far as possible for six minutes. You will walk back and forth. Six minutes is a long time to walk, so you will be exerting yourself. You may get out of breath, or find it exhausting. You should walk and not run, at your own pace. You can slow down, stop, and rest as you need to. You may lean against the wall, or sit down while resting.\n\nYou should perform the test on a level, flat surface. You will need to set a start point and a point around which you will walk. You should try to make this distance as close to 30 meters as possible. You should perform the test in someone else’s presence, so they can call for assistance if required."
+        label.text = "Before we begin the test you should place two markers as close to 30 meters apart as possible. You should perform the test on a hard, flat surface. You should perform the test in someone else’s presence so they can assist you if needed.\n\nThe aim of this test is to walk as far as possible for 6 minutes. You will walk between the two markers you have set, as many times as you can. I will let you know as each minute goes past, and then at 6 minutes I will ask you to stop where you are. 6 minutes is a long time to walk, so you will be exerting yourself. You are permitted to slow down, to stop, and to rest as necessary, but please resume walking as soon as you are able. Remember that the objective is to walk as far as possible for 6 minutes, but don’t run or jog."
+        
+        second_label_heading.text = "Before you begin.."
             
-            button.translatesAutoresizingMaskIntoConstraints = false
-            button.widthAnchor.constraint(equalToConstant: 100).isActive = true
-            button.heightAnchor.constraint(equalToConstant: 40).isActive = true
-            button.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
-            button.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 20).isActive = true
-            button.setTitle("Next", for: .normal)
-        case 1:
-            label.text = "You must not complete this test if you have had a heart attack or suffered angina in the last month.\n\nIf you develop chest pain, intolerable breathlessness, leg cramps, if you start staggering, severely sweating or someone says you look unwell, you must stop the test immediately."
+        second_label.text = "Press the \"Close\" button in the top right-hand corner of the screen to close the instructions and return to the main page. From there you will be able to begin the walk test. Upon pressing the \"Begin\" button, you will be told to stand still, face the direction you will be walking, place your phone in your pocket or in your hand by your side, and a countdown will commence. When the countdown finishes and you hear the word go, start walking."
             
-            second_label_heading.text = "Before you begin.."
+        contentView.addSubview(second_label_heading)
+        contentView.addSubview(second_label)
             
-            second_label.text = "Press the \"Close\" button in the top right-hand corner of the screen to close the instructions and return to the main page. From there you will be able to begin the walk test. Upon pressing the \"Begin\" button, you will be told to stand still, face the direction you will be walking, place your phone in your pocket or in your hand by your side, and a countdown will commence. When the countdown finishes and you hear the word go, start walking."
+        second_label_heading.translatesAutoresizingMaskIntoConstraints = false
+        second_label_heading.widthAnchor.constraint(equalTo: contentView.widthAnchor, constant: -40).isActive = true
+        second_label_heading.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 20).isActive = true
+        second_label_heading.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
             
-            contentView.addSubview(second_label_heading)
-            contentView.addSubview(second_label)
-            
-            second_label_heading.translatesAutoresizingMaskIntoConstraints = false
-            second_label_heading.widthAnchor.constraint(equalTo: contentView.widthAnchor, constant: -40).isActive = true
-            second_label_heading.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 20).isActive = true
-            second_label_heading.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
-            
-            second_label.translatesAutoresizingMaskIntoConstraints = false
-            second_label.widthAnchor.constraint(equalTo: contentView.widthAnchor, constant: -40).isActive = true
-            second_label.topAnchor.constraint(equalTo: second_label_heading.bottomAnchor, constant: 20).isActive = true
-            second_label.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
-        default:
-            return
-        }
+        second_label.translatesAutoresizingMaskIntoConstraints = false
+        second_label.widthAnchor.constraint(equalTo: contentView.widthAnchor, constant: -40).isActive = true
+        second_label.topAnchor.constraint(equalTo: second_label_heading.bottomAnchor, constant: 20).isActive = true
+        second_label.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
         
         let padding_bottom = UIView()
         contentView.addSubview(padding_bottom)
         padding_bottom.translatesAutoresizingMaskIntoConstraints = false
-        switch page {
-        case 0:
-            padding_bottom.topAnchor.constraint(equalTo: button.bottomAnchor, constant: 25).isActive = true
-        case 1:
-            padding_bottom.topAnchor.constraint(equalTo: second_label.bottomAnchor, constant: 25).isActive = true
-        default:
-            return
-        }
+        padding_bottom.topAnchor.constraint(equalTo: second_label.bottomAnchor, constant: 25).isActive = true
         padding_bottom.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
     }
     
@@ -192,16 +131,8 @@ class WalkTestInstructionController: UIViewController {
     //                                 //
     /////////////////////////////////////
     override func viewDidAppear(_ animated: Bool) {
-        switch page {
-        case 0:
-            audioManager.playAudio(name: "instruction-p1-pt1")
-            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(playNext), userInfo: nil, repeats: true)
-        case 1:
-            audioManager.playAudio(name: "instruction-p2-pt1")
-            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(playNext), userInfo: nil, repeats: true)
-        default:
-            return
-        }
+        audioManager.playAudio(name: "instruction-pt1")
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(playNext), userInfo: nil, repeats: true)
     }
     
     //////////////////////////////////////////////////
