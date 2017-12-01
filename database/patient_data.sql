1fb6c1c81cb21715b2690ae1697c371f2bf09d94
chris <chris@chriss-MacBook.local>
2017-12-02 00:21:34 +1030

    Whoops, only added the health_data table. Was meant to update the patient_data database.

--- database/patient_data.sql
+++ database/patient_data.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 01, 2017 at 02:48 PM
+-- Generation Time: Dec 01, 2017 at 02:50 PM
 -- Server version: 5.6.35
 -- PHP Version: 7.1.8
 
@@ -29,6 +29,64 @@ CREATE TABLE `health_data` (
   `distance` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `height_weight_data`
+--
+
+CREATE TABLE `height_weight_data` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `unique_id` varchar(50) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `height` varchar(10) NOT NULL,
+  `weight` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `location_data`
+--
+
+CREATE TABLE `location_data` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `unique_id` varchar(50) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `latitude` decimal(8,6) NOT NULL,
+  `longitude` decimal(9,6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `survey_data`
+--
+
+CREATE TABLE `survey_data` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `unique_id` varchar(50) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `question` varchar(100) NOT NULL,
+  `answer` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `walk_test_data`
+--
+
+CREATE TABLE `walk_test_data` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `unique_id` varchar(50) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `steps` int(11) NOT NULL,
+  `distance` int(11) NOT NULL,
+  `laps` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 --
 -- Indexes for dumped tables
 --
@@ -40,6 +98,30 @@ ALTER TABLE `health_data`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `height_weight_data`
+--
+ALTER TABLE `height_weight_data`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `location_data`
+--
+ALTER TABLE `location_data`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `survey_data`
+--
+ALTER TABLE `survey_data`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `walk_test_data`
+--
+ALTER TABLE `walk_test_data`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -47,4 +129,24 @@ ALTER TABLE `health_data`
 -- AUTO_INCREMENT for table `health_data`
 --
 ALTER TABLE `health_data`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `height_weight_data`
+--
+ALTER TABLE `height_weight_data`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `location_data`
+--
+ALTER TABLE `location_data`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `survey_data`
+--
+ALTER TABLE `survey_data`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `walk_test_data`
+--
+ALTER TABLE `walk_test_data`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
